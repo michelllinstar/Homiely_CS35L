ab0aff53c144ae8ba97ef2a4bf12d72496c94e66
TomLe1227 <2005thomasle@gmail.com>
2026-05-10 22:15:12 -0700

    made a demo flow for all pages to navigate between pages better

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DemoFlow from "./components/DemoFlow";
 
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
+
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
       </Routes>
+
+      <DemoFlow />
     </BrowserRouter>
   );
 }
--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,19 +1,22 @@
 import "./Landing.css";
-import Button from '../../components/Button';
+import Button from "../../components/Button";
 
 export default function Landing() {
+  
+
   return (
-    
     <div className="landing-page">
-      <h1 className="title">Welcome to Homiely</h1>
+      <div className="landing-content">
+        <h1 className="title">Welcome to Homiely</h1>
 
-      <p className="subtitle">
-        Track chores, split expenses, and keep roommate life peaceful.
-      </p>
+        <p className="subtitle">
+          Track chores, split expenses, and keep roommate life peaceful.
+        </p>
 
-      <div className="button-row">
-        <Button label="Sign in" to="/Signup"/>
-        <Button label="Log in" to="/login" />
+        <div className="button-row">
+          <Button label="Sign up" to="/Signup" />
+          <Button label="Log in" to="/login" />
+        </div>
       </div>
     </div>
   );
