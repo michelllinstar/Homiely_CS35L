626e69a3be033c4effee5dc692441548dc05f610
TomLe1227 <2005thomasle@gmail.com>
2026-05-10 21:43:37 -0700

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
 
 function App() {
   return (
