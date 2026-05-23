4e561574bdc930c26a70ae406cb027a5fa7003ca
michelllinstar <140552879+michelllinstar@users.noreply.github.com>
2026-05-23 15:10:30 -0700

    add calendar page

---
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -0,0 +1,21 @@
+import "./Calendar.css";
+import {useState} from "react";
+import {useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import CalendarChoreMo from "../../components/CalendarChoreMo";
+import CalendarAvailMo from "../../components/CalendarAvailMo";
+
+
+export default function Calendar() {
+
+    return (
+        <div className="calendar-page">
+            <header>
+                <h1 className="calendar-title">Availability</h1>
+                
+                <CalendarAvailMo />
+                <CalendarChoreMo />
+            </header>
+        </div>
+    );
+}
